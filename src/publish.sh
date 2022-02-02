de003fc2d3a60e0fdbf9af8375a8afffae3b92f7
jochemste <jochemstevense@protonmail.com>
2022-02-02 09:12:41 +0100

    fix: Changed the container name grabbing part of publish.sh and updated .gitignore

--- src/publish.sh
+++ src/publish.sh
@@ -4,9 +4,6 @@ REPO_TOKEN="$3"
 
 GITHUB_REPOSITORY="${GITHUB_REPOSITORY,,}"
 
-echo "VERSION=$VERSION"
-echo "OVERRIDE=$OVERRIDE"
-
 docker login ghcr.io -u ${GITHUB_REF} -p ${REPO_TOKEN}
 
 VERSION=$VERSION docker-compose -f docker-compose.yml -f $OVERRIDE up --no-start --remove-orphans
@@ -15,10 +12,17 @@ IMAGES=$(docker inspect --format='{{.Image}}' $(docker ps -aq))
 echo "IMAGES: $IMAGES"
 for IMAGE in $IMAGES; do
     echo "IMAGE: $IMAGE"
-    
-    NAME=$(basename ${GITHUB_REPOSITORY}).$(docker inspect --format '{{ index .Config.Labels "name" }}' $IMAGE)
-    TAG="ghcr.io/${GITHUB_REPOSITORY}/$NAME:$VERSION"
 
-    docker tag $IMAGE $TAG
-    docker push $TAG
+    APPEND=$(docker inspect --format '{{ index .RepoTags }}' $IMAGE 2>/dev/null | grep -oP '(?<=\[).*?(?=\:)') 
+    echo $?
+    if [ $? -eq 0 ]; then
+	if [ -n "$APPEND" ]; then
+	    NAME=$(basename ${GITHUB_REPOSITORY}).$APPEND
+	    TAG="ghcr.io/${GITHUB_REPOSITORY}/$NAME:$VERSION"
+	    echo "Tagging and pushing $TAG"
+
+	    docker tag $IMAGE $TAG
+	    docker push $TAG
+	fi
+    fi
 done
